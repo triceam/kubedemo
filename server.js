86d65ac07cfc49822fe4090d9acd616c68985493
ANDREW M. TRICE <amtrice@us.ibm.com>
2017-05-22 15:49:59 -0400

    removed sample result

--- server.js
+++ server.js
@@ -24,16 +24,7 @@ app.use("/explorer", express.static("./public/swagger-ui"));
 // Business logic
 app.get("/products", function(req, res, next){
 
-	res.json([{
-		"id":0,
-		"name":"IBM"
-	},{
-		"id":1,
-		"name":"Bluemix"
-	},{
-		"id":2,
-		"name":"Watson"
-	}]);
+	res.json([]);
 });
 
 app.post("/products", function(req, res, next){
